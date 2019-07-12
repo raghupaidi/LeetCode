81a1530349f0878f248e1f76896a4018e60ada1e
raghupaidi <Manikanta@2012>
2019-07-12 13:45:18 -0400

    Testing ShapeFactory Commit

---
+++ PatternExperiments/FactoryPattern/ShapeFactory.cs
@@ -0,0 +1,36 @@
+﻿using PatternExperiments.FactoryPattern.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternExperiments.FactoryPattern
+{
+    public class ShapeFactory
+    {
+        public Shape GetShape(String shapeType)
+        {
+            Shape result= new Circle();
+
+            if (string.IsNullOrWhiteSpace(shapeType))
+            {
+                return null;
+            }
+
+            switch (shapeType)
+            {
+                case "Circle":
+                    result= new Circle();
+                    break;
+                case "Rectangle":
+                    result= new Circle();
+                    break;
+                case "Square":
+                    result= new Circle();
+                    break;
+        }
+
+            return result;
+    }
+}
